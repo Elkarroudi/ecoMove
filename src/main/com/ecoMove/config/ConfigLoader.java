e5f5be95e8d6672653e90b5ed8a6566f77ca1fbe
Elkarroudi <amineelkarroudi04@proton.me>
2024-09-02 20:12:03 +0100

    Finish Config Loader and env.

---
+++ src/main/com/ecoMove/config/ConfigLoader.java
@@ -0,0 +1,24 @@
+package main.com.ecoMove.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+
+    private Properties env;
+    public ConfigLoader() throws IOException {
+        env = new Properties();
+        try (InputStream inputFile = getClass().getResourceAsStream("/main/com/ecoMove/config/env.properties")) {
+            if (inputFile == null) {
+                System.out.println("Sorry, unable to find env.properties");
+                return;
+            }
+            env.load(inputFile);
+        }
+    }
+
+    public String getProperty(String key) {
+        return env.getProperty(key);
+    }
+}
